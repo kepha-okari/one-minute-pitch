f497e2704a456832e79da40238a725684c72868c
rkepha <kephaokari@gmail.com>
2017-12-23 23:36:32 +0300

    added license and User model

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 from app import create_app, db
 from flask_script import Manager, Server
+from app.models import User
 from flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
@@ -22,7 +23,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role)
+    return dict(app=app, db=db, User=User)
     pass
 
 
