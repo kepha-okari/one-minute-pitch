36dd6e5b5440f59ccf94f5a03b09c71bc1200bcc
rkepha <kephaokari@gmail.com>
2018-01-17 18:24:29 +0300

    deploy to heroku

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export SECRET_KEY='try harder'
 export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://rkepha:KR@localhost/pitch'
-export MAIL_USERNAME='kephaokari@gmail.com'
+export MAIL_USERNAME='rkapps47@gmail.com'
 export MAIL_PASSWORD='Krom0147'
 python3.6 manage.py server
